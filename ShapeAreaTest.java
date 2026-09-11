737a0ed778a8226323f157df7217a4d8664a3f61
smamgain555-hash <smamgain555@gmail.com>
2026-09-11 11:37:24 +0530

    Create ShapeAreaTest.java

---
+++ ShapeAreaTest.java
@@ -0,0 +1,13 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[2];
+
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+
+        for (Shape s : shapes) {
+            s.displayArea();
+        }
+    }
+}
